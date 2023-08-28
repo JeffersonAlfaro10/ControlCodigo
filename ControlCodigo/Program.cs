671449262a748e3dd38170a1af048ce7a20b68d9
jeffersonalfaro260696@gmail.com <jeffersonalfaro260696@gmail.com>
2023-08-28 16:57:25 -0600

    Revert "deja solo un mensaje"
    This reverts commit 57365827998a3ddc5d936d847fc19e112bef2f9c.

--- ControlCodigo/Program.cs
+++ ControlCodigo/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("hola, prueba git// hola mundo");
-
+Console.WriteLine("hola, prueba git// hola mundo");
+Console.WriteLine("hola, prueba git// hola mundo");
+Console.WriteLine("hola, prueba git// hola mundo");
+Console.WriteLine("hola, prueba git// hola mundo");
+Console.WriteLine("hola, prueba git// hola mundo");
